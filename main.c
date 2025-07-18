d8484da4998c41627f452ae89836f9cc2e7c7eb5
Efe Keskin <158193414+efekeskn@users.noreply.github.com>
2025-07-18 08:44:35 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,44 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-
-typedef struct ogrenci{
-    char isim[20];
-    char soyisim[20];
-    int no;
-    int notlar[3];
-} ogrenci;
-
-
-
-
-int main()
-{
-    FILE *fptr=fopen("efe.txt","r");
-    ogrenci ogrenciler[3];
-    int i=0;
+int main() {
+    FILE* fptr = fopen("efe.txt", "r");
     
-    while(i<3)
-    {
-        fscanf(fptr,"%s%s%d%d%d%d",ogrenciler[i].isim,ogrenciler[i].soyisim,&ogrenciler[i].no,&ogrenciler[i].notlar[0],&ogrenciler[i].notlar[1],&ogrenciler[i].notlar[2]);
-        i++;
-        
+    if (fptr == NULL) {
+        printf("Dosya açılamadı.\n");
+        return 1;
     }
     
-    for(int j=0;j<3;j++)
-    {
-        printf("Ad: %s\n",ogrenciler[j].isim);
-        printf("SoyAd: %s\n",ogrenciler[j].soyisim);
-        printf("ogrenci no:%d\n",ogrenciler[j].no);
-        printf("notlar: %d  %d  %d",ogrenciler[j].notlar[0],ogrenciler[j].notlar[1],ogrenciler[j].notlar[2]);
-        
-        printf("\n\n\n");
+    char c;
+    int count = 0;
+    
+    while ((c = fgetc(fptr)) != EOF) {
+        if (c == ' ') {
+            count++;
+        }
     }
     
+    printf("Boşluk karakteri sayısı: %d\n", count);
     
-    
-    
+    fclose(fptr);
 
     return 0;
 }
