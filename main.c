1f157e39d896ff64ae8c7fc8ceca14e47a09ea5b
Efe Keskin <158193414+efekeskn@users.noreply.github.com>
2025-07-18 08:45:03 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,25 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main() {
-    FILE* fptr = fopen("efe.txt", "r");
-    
-    if (fptr == NULL) {
-        printf("Dosya açılamadı.\n");
-        return 1;
-    }
-    
-    char c;
-    int count = 0;
-    
-    while ((c = fgetc(fptr)) != EOF) {
-        if (c == ' ') {
-            count++;
-        }
-    }
-    
-    printf("Boşluk karakteri sayısı: %d\n", count);
-    
-    fclose(fptr);
+/*
+fprintf(): Dosyaya biçimlendirilmiş veri yazmak için kullanılır.    
+    int fprintf(FILE *stream, const char *format, ...) şeklinde kullanılır.
+fscanf(): Dosyadan biçimlendirilmiş veri okumak için kullanılır.
+    int fscanf(FILE *stream, const char *format, ...) şeklinde kullanılır.
+fgetc(): Dosyadan bir karakter okumak için kullanılır. 
+    int fgetc(FILE *stream) şeklinde kullanılır.
+fputc(): Dosyaya bir karakter yazmak için kullanılır. 
+    int fputc(int character, FILE *stream) şeklinde kullanılır.
+fgets(): Dosyadan bir satır okumak için kullanılır. 
+    char *fgets(char *str, int n, FILE *stream) şeklinde kullanılır.
+fputs(): Dosyaya bir satır yazmak için kullanılır. 
+    int fputs(const char *str, FILE *stream) şeklinde kullanılır.
+*/
 
-    return 0;
+
+int main()
+{
+    FILE* fptr=fopen("efe.txt","r");
+    
+    int max=0;
+    char num[20];
+    
+    
+    while(fscanf(fptr, "%s", num) != EOF)
+    {
+        int a=atoi(num);
+        if(a>max)
+            max=a;
+        
+    }
+    printf("%d",max);
+    
 }
