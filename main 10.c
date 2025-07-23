d7ff496c079d700fbfe4a64cca0052001c6b6816
Efe Keskin <158193414+efekeskn@users.noreply.github.com>
2025-07-23 12:07:01 +0300

    Add files via upload

---
+++ main 10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// c programlama alıştırmaları
+
+int main()
+{
+    short sayi4;
+    int  sayi1=188;
+    int bosluk=' ';
+    char efe[199];
+    char e[20]="9799";
+    sayi1=atoi(e);// atol-->long long  atof-->float   
+    printf("%d",sayi1);
+    
+    long long sayi2=30;
+    
+    
+    
+
+    return 0;
+}
