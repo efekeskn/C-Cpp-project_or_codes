da68a59921f7f71babbb0b643b72122e1fbddad1
Efe Keskin <158193414+efekeskn@users.noreply.github.com>
2025-07-29 23:24:52 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,60 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
 
-typedef struct queue {
-    int val;
-    struct queue* next;
-} Queue;
-
-Queue* front = NULL;
-Queue* rear = NULL;
-
-void enqueue(int n) {
-    Queue* temp = (Queue*)malloc(sizeof(Queue));
-    if (temp == NULL) {
-        printf("Bellek tahsisi basarisiz.");
-        exit(1);
-    }
-    temp->val = n;
-    temp->next = NULL;
-    if (front == NULL)
-        front = rear = temp;
-    else {
-        rear->next = temp;
-        rear = temp;
+// yazılan kelimeyi ve cümleyi tersten yazdırma
+void kelimeleriterstenyazdir(char a[])
+{
+    int b,c;
+    b=strlen(a);
+    for(c=b;c>=0;c--)
+    {
+        if(a[c]==' ' || c==0)
+        {
+            for(int i=c;i<b;i++)
+            {
+                printf("%c",a[i]);
+            }
+            b=c;
+        }
     }
 }
 
-void dequeue() {
-    if (front == NULL) {
-        printf("Queue bos.");
-        exit(1);
+void cumleyiterstenyazdir(char a[])
+{
+    int c=strlen(a);
+    for(;c>0;c--)
+    {
+        printf("%c",a[c-1]);
     }
-    printf("Silinen deger: %d\n", front->val);
-    Queue* temp = front;
-    front = front->next;
-    free(temp);
 }
 
-int main() {
-    srand(time(NULL));
-
-    int n = rand() % 10 + 1; // Rasgele 1 ile 10 arasında bir sayı seç
-    int total_time = 0;
-
-    for (int i = 0; i < n; i++) {
-        int a = rand() % 5; // Her katta rastgele 0 ile 4 arasında bir sayı seç
-        printf("%d. katta binecek kisi sayisi: %d\n", i + 1, a);
-        enqueue(a);
-        total_time += 2 * a + 3; // Her kişi 2 saniyede biner ve kat araları 3 saniyede çıkar
+int main()
+{
+    char cumle[100];
+    int a;
+    
+    printf("lutfen cumlenizi yaziniz:");
+    fgets(cumle,100,stdin);
+    
+    printf("lutfen yapmak istediginiz işlemi giriniz: kelimeleri tersten yazdır için '1' cumleyi tersten yazdırmak için '2' :" );
+    scanf("%d",&a);
+    
+    if(a==1)
+    {
+        kelimeleriterstenyazdir(cumle);
+    }    
+    else if(a==2)
+    {
+       cumleyiterstenyazdir(cumle);
     }
-
-    while (front != NULL) {
-        dequeue();
-    }
-
-    printf("\nToplam gecirilen zaman: %d", total_time - 3); // Son kat arası çıkış süresi düzeltiliyor
-
+    
+    
     return 0;
 }
