c78c0cb75d2ff109895a8c59bbe320ba67e6519f
Efe Keskin <158193414+efekeskn@users.noreply.github.com>
2025-07-30 10:29:38 +0300

    Add files via upload

--- main 7.c
+++ main 7.c
@@ -1,85 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+int climbStairs(int n) {
 
-typedef struct queue{
-    
-    int val;
-    struct queue* next;
-    
-}Queue;
-
-Queue* front=NULL;
-Queue* rear=NULL;
-
-Queue* que=NULL;
-
-void newqueue(int n)
-{
-    Queue* temp=(Queue*)malloc(sizeof(Queue));
-    
-    temp->val=n;
-    temp->next=NULL;
-    front=temp;
-    rear=temp;
-    
-}
-
-void enqueue(int n)
-{
-    if(front==NULL || rear==NULL)
-    {
-        newqueue(n);
-        return;
+    if (n <= 1) {
+        return 1;
     }
-    
-    Queue* temp=(Queue*)malloc(sizeof(Queue));
-    
-    temp->val=n;
-    temp->next=NULL;
-    rear->next=temp;
-    rear=rear->next;
-}
-
-void dequeue()
-{
-    if(front==NULL)
-    {
-        printf("the quee is empty.");
-        exit(1);
+    int step1 = 1, step2 = 1, current;
+     for (int i = 1; i < n; i++) {
+        current = step1 + step2;
+        step1 = step2;
+        step2 = current;
     }
-    
-    printf("silinen deger: %d\n",front->val);
-    Queue* temp=front;
-    front=front->next;
-    temp->next=NULL;
-    
-   // free(temp);
+    return current;
 }
-
-
-int main()
-{
-    srand(time(NULL));
-    
-    int n=rand()%10+1,tot_time=0;
-    
-    for(int i=0;i<n;i++)
-    {
-        int a=rand()%5;
-        printf("%d. katta binecek kişi sayisi:%d\n",i+1,a);
-        enqueue(a);
-        tot_time+=2*a+3; //her kisi iki saniyede binmektedir ve kat aralarini 3 saniyede cikar
-    }
-    
-    while(front!=NULL)
-    {
-        dequeue();
-    }
-    
-    printf("\n\ntoplam geçirilen zaman: %d",tot_time-3);
-    
-    
-    return 0;
-}
-
--- main 8.c
+++ main 8.c
@@ -1,26 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void display(char std[]);
+int main() {
+    int satir, sutun;
+    printf("Lutfen matrisin satir ve sutunlarini giriniz:");
+    scanf("%d%d", &satir, &sutun);
 
+    int spiralmatris[satir][sutun];
+    int sol = 0, yukari = 0, deger = 1, sag = sutun - 1, asagi = satir - 1;
+    int gosterge = satir * sutun;
 
-int main()
-{
-    char c[10];
-    int a=0;
-    fgets(c,sizeof(c),stdin);
-    
-    
-    
-    printf("%s",c);
-    
-    puts("efe keskin");
-    display(c);
-    
+    while (deger <= gosterge) {
+        // Saga git 
+        for (int i = sol; i <= sag && deger <= gosterge; i++)
+            spiralmatris[yukari][i] = deger++;
+        yukari++;
+
+        // Asagi git
+        for (int i = yukari; i <= asagi && deger <= gosterge; i++)
+            spiralmatris[i][sag] = deger++;
+        sag--;
+
+        // Sola git
+        for (int i = sag; i >= sol && deger <= gosterge; i--)
+            spiralmatris[asagi][i] = deger++;
+        asagi--;
+
+        // Yukari git
+        for (int i = asagi; i >= yukari && deger <= gosterge; i--)
+            spiralmatris[i][sol] = deger++;
+        sol++;
+    }
+
+    // Matrisi yazdir
+    for (int i = 0; i < satir; i++) {
+        for (int j = 0; j < sutun; j++) {
+            printf("%d\t", spiralmatris[i][j]);
+        }
+        printf("\n\n");
+    }
+
     return 0;
 }
 
-void display(char str[])
-{
-    printf("%s",str);
-}
