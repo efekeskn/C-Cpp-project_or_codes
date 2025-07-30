fd74ed56997a215dbbba8a4752967a01db94d886
Efe Keskin <158193414+efekeskn@users.noreply.github.com>
2025-07-30 10:30:48 +0300

    Add files via upload

--- main 2.c
+++ main 2.c
@@ -1,80 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct queue{
-    
-    int val;
-    struct queue* next;
-}Queue;
+char* intToRoman(int num) {
+    // İhtiyacımız olan maksimum Roma rakamı uzunluğu tahmin edilemez,
+    // bu yüzden dinamik bellek tahsis ediyoruz.
+    char *rom = (char *)malloc(20 * sizeof(char));
+    int i = 0;
 
-Queue* front=NULL;
-Queue* rear=NULL;
-
-Queue* newnode(int n)
-{
-    Queue* temp=(Queue*)malloc(sizeof(Queue));
-    if(temp==NULL)
-        exit(1);
-    temp->val=n;
-    front=temp;
-    rear=temp;
-    temp->next=NULL;
-    return temp;
-}
-void enqueue(int n)
-{
-    Queue* temp=(Queue*)malloc(sizeof(Queue));
-    if(temp==NULL)
-        exit(1);
-    temp->val=n;
-    temp->next=NULL;
-    rear->next=temp;
-    rear=rear->next;
-}
-int dequeue_and_time()
-{
-    Queue* temp=(Queue*)malloc(sizeof(Queue));
-    int a;
-    
-    if(front==NULL)
-    {
-        printf("queue is empty.");
-        exit(1);
+    while (num > 0) {
+        if (num >= 1000) {
+            rom[i++] = 'M';
+            num -= 1000;
+        } else if (num >= 900) {
+            rom[i++] = 'C';
+            rom[i++] = 'M';
+            num -= 900;
+        } else if (num >= 500) {
+            rom[i++] = 'D';
+            num -= 500;
+        } else if (num >= 400) {
+            rom[i++] = 'C';
+            rom[i++] = 'D';
+            num -= 400;
+        } else if (num >= 100) {
+            rom[i++] = 'C';
+            num -= 100;
+        } else if (num >= 90) {
+            rom[i++] = 'X';
+            rom[i++] = 'C';
+            num -= 90;
+        } else if (num >= 50) {
+            rom[i++] = 'L';
+            num -= 50;
+        } else if (num >= 40) {
+            rom[i++] = 'X';
+            rom[i++] = 'L';
+            num -= 40;
+        } else if (num >= 10) {
+            rom[i++] = 'X';
+            num -= 10;
+        } else if (num >= 9) {
+            rom[i++] = 'I';
+            rom[i++] = 'X';
+            num -= 9;
+        } else if (num >= 5) {
+            rom[i++] = 'V';
+            num -= 5;
+        } else if (num >= 4) {
+            rom[i++] = 'I';
+            rom[i++] = 'V';
+            num -= 4;
+        } else if (num >= 1) {
+            rom[i++] = 'I';
+            num -= 1;
+        }
     }
-    temp=front;
-    front=front->next;
-    temp->next=NULL;
-    a=temp->val;
-    free(temp);
-    return a;
+    // Sonlandırıcı karakter ekleyelim
+    rom[i] = '\0';
+    return rom;
 }
 
-
-
-int main()
-{
-    int n,zaman,top_zaman=0;
-    
-    printf("lutfen sirada kac kisinin oldugunu yaziniz:");
-    scanf("%d",&n);
-    
-    printf("lutfen 1. kisinin bekleme suresini giriniz (dk):");
-        scanf("%d",&zaman);
-    Queue* que=newnode(zaman);
-    
-    
-    for(int i=1;i<n;i++)
-    {
-        printf("lutfen %d. kisinin bekleme suresini giriniz (dk):",i+1);
-        scanf("%d",&zaman);
-        enqueue(zaman);
-    }
-    for(int i=0;i<n;i++)
-    {
-        top_zaman+=dequeue_and_time();
-    }
-    
-    printf("toplam beklnecek sure:%d",top_zaman);
-    
+int main() {
+    int num;
+    printf("Bir tamsayi girin: ");
+    scanf("%d", &num);
+    char *roman = intToRoman(num);
+    printf("Roma rakami: %s\n", roman);
+    // Belleği serbest bırakalım
+    free(roman);
     return 0;
 }
