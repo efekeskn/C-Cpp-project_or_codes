8e046de0abff37b2621d2ec2b7c41e7801c66586
Efe Keskin <158193414+efekeskn@users.noreply.github.com>
2025-07-30 10:30:32 +0300

    Add files via upload

--- main 3.c
+++ main 3.c
@@ -1,17 +1,13 @@
-//1 1 2 3 5 8 13 21 34 55 
-#include <stdlib.h>
 #include <stdio.h>
-int fibonacci(int a)
-{
-    if(a==1 || a==2)
-    return 1;
-   return fibonacci(a-1)+fibonacci(a-2);
-}
 
-int main()
-{
-    int a=fibonacci(7);
-
-    printf("%d-",a);
+int main() {
+    char str[] = "12 4.5";
+    int num;
+    float fnum;
+    
+    sscanf(str, "%d %f", &num, &fnum);
+    
+    printf("Integer: %d, Float: %f\n", num, fnum);
+    
     return 0;
 }
